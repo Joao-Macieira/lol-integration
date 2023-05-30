35a0973ad00b2b95f61afca480f20bf22178b9ac
João Macieira <jp.macieira@uol.com.br>
2023-05-30 17:11:27 -0300

    feat: config postgres

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -6,11 +6,11 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'lol-api',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       });
